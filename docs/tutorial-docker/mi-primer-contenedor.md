4fb5beffaecd5035899db4b320442137e751200c
Javier Núñez <javierignacio.nunez@gmail.com>
2024-12-30 19:22:17 -0300

    primer commit tuto

---
+++ docs/tutorial-docker/mi-primer-contenedor.md
@@ -0,0 +1,28 @@
+---
+sidebar_position: 3
+---
+
+
+# 4. Mi primer Containerrr 🎤 
+
+## Recuerdo cuando del container yo me enamoré 🎶
+
+Es importantisimo que tengas en ejecución Docker Desktop, asi que asegurate que le diste doble click y está funcionando.
+
+**Hello World desde Docker 🐳**
+
+Ahora abre la terminal y escribe el siguiente comando
+
+```bash
+docker run hello-world
+```
+Si todo está bien deberias ver el **Hello from Docker** en tu terminal.
+
+![hello world docker](./img/hello_world.png)
+
+**¡Felicidades echaste a andar tu primer contenedor!** gracias por revisar este tutorial y nos vemos en el siguiente! 👋
+
+Estaba bromeando, ya ejecutaste tu primer contenedor, ahora vamos a ver los conceptos básicos para que ejecutemos más contenedores.
+
+
+
---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,130 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+// const FeatureList = [
+//   {
+//     title: 'Fácil de Aprender',
+//     Svg: require('@site/static/img/aprendizaje.svg').default,
+//     description: (
+//       <>
+//         Todo está diseñado para que puedas aprender de manera simple y rápida.
+//         Cada tutorial está explicado paso a paso, ideal para avanzar sin
+//         complicaciones.
+//       </>
+//     ),
+//   },
+//   {
+//     title: 'Centrate en lo Importante',
+//     Svg: require('@site/static/img/concentracion.svg').default,
+//     description: (
+//       <>
+//         Aquí encuentras lo esencial. Olvídate de perder tiempo buscando
+//         información desordenada y enfócate en aplicar lo aprendido a tus tareas
+//         académicas, proyectos personales o laborales.
+//       </>
+//     ),
+//   },
+//   {
+//     title: 'Tecnología a tu Alcance',
+//     Svg: require('@site/static/img/tecnologia.svg').default,
+//     description: (
+//       <>
+//         Usamos las mejores herramientas y tecnologías para asegurarnos de que
+//         aprendas con ejemplos prácticos que puedes aplicar en el mundo real.
+//       </>
+//     ),
+//   },
+// ];
+
+// function Feature({Svg, title, description}) {
+//   return (
+//     <div className={clsx('col col--4')}>
+//       <div className="text--center">
+//         <Svg className={styles.featureSvg} role="img" />
+//       </div>
+//       <div className="text--center padding-horiz--md">
+//         <Heading as="h3">{title}</Heading>
+//         <p>{description}</p>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default function HomepageFeatures() {
+//   return (
+//     <section className={styles.features}>
+//       <div className="container">
+//         <div className="row">
+//           {FeatureList.map((props, idx) => (
+//             <Feature key={idx} {...props} />
+//           ))}
+//         </div>
+//       </div>
+//     </section>
+//   );
+// }
+
+
+const FeatureList = [
+  {
+    title: 'Fácil de Aprender',
+    imgSrc: '/img/aprendizaje.svg',
+    description: (
+      <>
+        Todo está diseñado para que puedas aprender de manera simple y rápida.
+        Cada tutorial está explicado paso a paso, ideal para avanzar sin
+        complicaciones.
+      </>
+    ),
+  },
+  {
+    title: 'Céntrate en lo Importante',
+    imgSrc: '/img/concentracion.svg',
+    description: (
+      <>
+        Aquí encuentras lo esencial. Olvídate de perder tiempo buscando
+        información desordenada y enfócate en aplicar lo aprendido a tus tareas
+        académicas, proyectos personales o laborales.
+      </>
+    ),
+  },
+  {
+    title: 'Tecnología a tu Alcance',
+    imgSrc: '/img/tecnologia.svg',
+    description: (
+      <>
+        Usamos las mejores herramientas y tecnologías para asegurarnos de que
+        aprendas con ejemplos prácticos que puedes aplicar en el mundo real.
+      </>
+    ),
+  },
+];
+
+function Feature({imgSrc, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <img src={imgSrc} className={styles.featureImage} alt={title} />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
